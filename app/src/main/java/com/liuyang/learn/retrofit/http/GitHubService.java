e75e61f12ed460a5d800b800bbd612b16e1500fb
liuyang581 <liuyang581@ke.com>
2021-01-12 15:31:25 +0800

    Retrofit一些注解的使用

--- app/src/main/java/com/liuyang/learn/retrofit/http/GitHubService.java
+++ app/src/main/java/com/liuyang/learn/retrofit/http/GitHubService.java
@@ -1,17 +1,118 @@
 package com.liuyang.learn.retrofit.http;
 
 import com.liuyang.learn.retrofit.model.User;
+import java.util.List;
+import java.util.Map;
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface GitHubService {
+
+  String BASE_URL = "https://api.github.com/";
+
+  /**
+   * 获取用户信息1
+   *
+   * @param user 用户名
+   */
   @GET("users/{user}")
   Call<User> getUser(@Path("user") String user);
 
+  /**
+   * 获取用户信息2
+   *
+   * @param user 用户名
+   */
   // 使用HTTP注解，可以代替其他几种注解
   // 如果是POST，hasBody改为true
   @HTTP(method = "GET", path = "users/{user}", hasBody = false)
   Call<User> getUser4Http(@Path("user") String user);
+
+  /**
+   * 获取一个用户的关注用户
+   * Query在链接后带一个参数
+   *
+   * @param user 用户名
+   * @param sort sortBy
+   */
+  @GET("users/{user}/following")
+  Call<List<User>> getUserFollowings(@Path("user") String user, @Query("sort") String sort);
+
+  /**
+   * 获取一个公司下的所有用户
+   *
+   * @param groupId 公司id
+   * @param options 参数
+   */
+  @GET("group/{id}/users")
+  Call<List<User>> groupList(@Path("id") int groupId,
+      @QueryMap Map<String, String> options);
+
+  /**
+   * 在Github上创建用户
+   *
+   * @param user 用户实体
+   *
+   * 使用Body注解，将User对象携带过去
+   */
+  @POST("users/new")
+  Call<User> createUser(@Body User user);
+
+  /**
+   * 更改用户名
+   *
+   * @param first firstName
+   * @param last lastName
+   *
+   * 使用Field注解配合FormUrlEncoded 提交表单的键值对信息
+   * FieldMap传递多个键值对信息
+   */
+  @FormUrlEncoded
+  @POST("user/edit")
+  Call<User> updateUser(@Field("first_name") String first, @Field("last_name") String last);
+
+  @Multipart
+  @PUT("user/photo")
+  Call<User> updateUser(@Part("photo") RequestBody photo,
+      @Part("description") RequestBody description);
+
+  /**
+   * Headers注解添加多个Header
+   *
+   * @param username 用户名
+   */
+  @Headers({
+      "Accept: application/vnd.github.v3.full+json",
+      "User-Agent: Retrofit-Sample-App"
+  })
+  @GET("user/{username}")
+  Call<User> getUser2(@Path("username") String username);
+
+  /**
+   * 使用Header注解添加不固定请求头
+   *
+   * @param lang 语言
+   * @param username 用户名
+   */
+  @GET("user/{username}")
+  Call<User> getUser3(@Header("Accept-Language") String lang, @Path("username") String username);
+
+  /**
+   * Streaming 作用于方法体。以流的形式返回，不会解析数据占用内存，当返回数据比较大时候使用
+   * URL 完整的请求URL，当baseURL不一致的时候可以使用
+   */
 }
---
+++ app/src/test/java/com/liuyang/learn/RetrofitTest.java
@@ -0,0 +1,73 @@
+package com.liuyang.learn;
+
+import com.liuyang.learn.retrofit.http.GitHubService;
+import com.liuyang.learn.retrofit.model.User;
+import java.io.File;
+import java.util.List;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static org.junit.Assert.*;
+
+public class RetrofitTest {
+
+  private Retrofit retrofit;
+  private GitHubService service;
+
+  @Before
+  public void setUp() throws Exception {
+    retrofit = new Retrofit.Builder()
+        .baseUrl(GitHubService.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+    service = retrofit.create(GitHubService.class);
+  }
+
+  @Test
+  public void testFollowing() throws Exception {
+    Call<List<User>> userFollowing = service.getUserFollowings("yi-zhe", "id");
+    Response<List<User>> response = userFollowing.execute();
+    List<User> users = response.body();
+    System.out.println("users: " + users.toString());
+  }
+
+  @Test
+  public void testNewUser() throws Exception {
+    Call<User> userFollowing = service.createUser(new User());
+    Response<User> response = userFollowing.execute();
+    User user = response.body();
+    System.out.println("user: " + user);
+  }
+
+  @Test
+  public void testFormSummit() throws Exception {
+    Call<User> user = service.updateUser("Jack", "Lucy");
+    User body = user.execute().body();
+    System.out.println("user: " + body);
+  }
+
+  @Test
+  public void testMultipart() throws Exception {
+    File file  = new File("launcher.png");
+    // 以数据流形式
+    MediaType mediaType = MediaType.parse("application/octet-stream");
+    RequestBody photo = RequestBody.create(mediaType, file);
+    // 以文件方式
+    RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "description");
+    Call<User> updateUser = service.updateUser(photo, description);
+    Response<User> response = updateUser.execute();
+    System.out.println("user: " + response.body());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    // release resource here
+  }
+}
