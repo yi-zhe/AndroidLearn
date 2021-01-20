02210deff14ceaa49d5ff6964c21551a654bb895
liuyang581 <liuyang581@ke.com>
2021-01-20 22:50:47 +0800

    ViewModel

--- app/src/main/java/com/liuyang/learn/jetpack/livedata/NameViewModel.java
+++ app/src/main/java/com/liuyang/learn/jetpack/livedata/NameViewModel.java
@@ -7,6 +7,24 @@ public class NameViewModel extends ViewModel {
 
   /**
    * 既是观察者也是被观察者
+   *
+   * ViewModelStore
+   *     private final HashMap<String, ViewModel> mMap = new HashMap<>();
+   *
+   * get(ViewModel.class) 通过反射创建对象，并缓存在mMap
+   *
+   * Activity调用onRetainCustomNonConfigurationInstance
+   *
+   * static final class NonConfigurationInstances {
+   *   Object activity;
+   *   HashMap<String, Object> children;
+   *   FragmentManagerNonConfig fragments;
+   *   ArrayMap<String, LoaderManager> loaders;
+   *   VoiceInteractor voiceInteractor;
+   * }
+   *
+   * 恢复数据 getLastNonConfigurationInstance()
+   * 保存数据 onRetainCustomNonConfigurationInstance()
    */
   private MutableLiveData<String> currentName;
   public int i;
